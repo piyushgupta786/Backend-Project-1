debc0f9e0fc6024002568056a898e1fd1798bbfd
piyush gupta <piyushguptacse786@gmail.com>
2026-06-25 19:24:28 +0530

    Backend Project 1

---
+++ BACKEND/src/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const multer = require('multer');
+
+const postModel = require('./models/post.model');
+const uplaodfile = require("./services/storage.service");
+
+const app = express();
+
+const uplaod = multer({ storage:multer.memoryStorage()})
+
+
+app.use(express.json());
+
+app.post('/create-post' ,uplaod.single('image'), async (req,res) =>{
+
+    console.log(req.body)
+    console.log(req.file)
+    const buffer = req.file.buffer;
+    
+    const result = await uplaodfile(buffer)
+
+    const post =  await postModel.create({
+        image: result.url,
+        caption: req.body.caption
+
+    }) 
+
+      return res.status(201).json({
+        message : "post uplaoded successfully",
+        post
+      })
+
+})
+
+app.get("/posts", async (req,res)=> {
+
+    const posts = await postModel.find()
+
+     return res.status(200).json({
+        message :"post fetched successfully",posts
+    })
+})
+
+module.exports = app;
